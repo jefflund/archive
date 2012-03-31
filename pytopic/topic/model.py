47bd3588c8d555cb24fbb4373a78dc92294c28fa
Jeffrey Lund <jlund3@maven>
2012-03-31 12:41:43 -0600

    Added topic package with TopicModel base class

---
+++ pytopic/topic/model.py
@@ -0,0 +1,23 @@
+"""Base class for graphical model"""
+
+class TopicModel(object):
+    """Base class for a topic model of textual data"""
+
+    def __init__(self, corpus):
+        self.titles = list(corpus.titles)
+        self.vocab = list(corpus.vocab)
+        self.w = [doc for doc in corpus]
+
+        self.M = len(self.w)
+        self.N = [len(doc) for doc in self.w]
+        self.V = len(self.vocab)
+
+    def iteration(self):
+        """
+        TopicModel.iteration(): return None
+        Performs a single iteration of inference
+        """
+
+    def inference(self, iterations):
+        for _ in range(iterations):
+            self.iteration()
