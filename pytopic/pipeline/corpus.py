389297e3f2bef1a0422688a5c3ef4d5e7280fb83
Jeffrey Lund <jlund3@maven>
2012-03-31 09:54:36 -0600

    Fixed CorpusReader and Tokenizer

--- pytopic/pipeline/corpus.py
+++ pytopic/pipeline/corpus.py
@@ -9,11 +9,14 @@ class Corpus(object):
     def __init__(self):
         self.vocab = Index()
         self.titles = Index()
-        self.data = []
+        self.data = {}
 
     def add_document(self, title, tokens):
         doc_index = self.titles.insert_token(title)
         self.data[doc_index] = self.vocab.convert_token(tokens)
+
+    def get_text(self, doc_index):
+        return [self.vocab[i] for i in self.data[doc_index]]
 
     def __getitem__(self, doc_index):
         return self.data[doc_index]
@@ -24,6 +27,7 @@ class Corpus(object):
     def __iter__(self):
         for doc_index in sorted(self.data.keys()):
             yield self.data[doc_index]
+
 
 class CorpusReader(object):
 
@@ -48,22 +52,23 @@ class CorpusReader(object):
     def read(self):
         corpus = Corpus()
         for filename, buff in self.get_files():
-            for title, tokens in self.tokenizer.read(filename, buff):
+            for title, tokens in self.tokenizer.tokenize(filename, buff):
                 corpus.add_document(title, tokens)
         return corpus
 
 
 class Tokenizer(object):
 
-    def __init__(self, stopwords=[], split_re='\s+', filter_re='[^a-zA-Z]'):
+    def __init__(self, stopwords, split_re='\s+', filter_re='[^a-zA-Z]'):
         self.stopwords = stopwords
         self.split_re = re.compile(split_re)
         self.filter_re = re.compile(filter_re)
 
     def tokenize(self, filename, buff):
-        tokens = self.split_re.split(buff)
+        tokens = self.split_re.split(buff.read())
         tokens = [self.transform(token) for token in tokens]
         tokens = [token for token in tokens if self.keep(token)]
+        yield filename, tokens
 
     def transform(self, token):
         return self.filter_re.sub('', token.lower())
