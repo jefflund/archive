fac5f8118fc875bdd30e64f004cac06d6d26a01d
Jeff Lund <jlund3@aml.cs.byu.edu>
2012-04-20 13:07:51 -0600

    Removed stopwords from NewsTokenizer

--- pytopic/pipeline/reader.py
+++ pytopic/pipeline/reader.py
@@ -5,8 +5,8 @@ from pytopic.pipeline.corpus import Tokenizer
 class NewsTokenizer(Tokenizer):
     """Tokenizer that skips newsgroups headers"""
 
-    def __init__(self, stopwords, split_re='\s+', filter_re='[^a-zA-Z]'):
-        Tokenizer.__init__(self, stopwords, split_re, filter_re)
+    def __init__(self, split_re='\s+', filter_re='[^a-zA-Z]'):
+        Tokenizer.__init__(self, split_re, filter_re)
 
     def tokenize(self, filename, buff):
         line = buff.readline()
