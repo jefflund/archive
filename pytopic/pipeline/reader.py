5bfc68904b37f40058e28d56761e9a1c13812e23
Jeff Lund <jlund3@aml.cs.byu.edu>
2012-05-07 14:59:36 -0600

    Added Bible tokenizer

--- pytopic/pipeline/reader.py
+++ pytopic/pipeline/reader.py
@@ -13,3 +13,23 @@ class NewsTokenizer(Tokenizer):
         while line.strip() != '':
             line = buff.readline()
         return Tokenizer.tokenize(self, filename, buff)
+
+
+class BibleTokenizer(Tokenizer):
+    """Tokenizer that treats each line as a title-document pair"""
+
+    def __init__(self, split_re='\s+', filter_re='[^a-zA-Z]'):
+        Tokenizer.__init__(self, split_re, filter_re)
+
+    def tokenize(self, filename, buff):
+        for line in buff:
+            line = line.strip()
+            if line == '':
+                continue
+
+            title, tokens = line.split(self.split_re, 1)
+            tokens = [self.transform(token) for token in tokens]
+            tokens = [token for token in tokens if self.keep(token)]
+            if len(tokens) > 0:
+                yield title, tokens
+
