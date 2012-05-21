0fedcdf34b3b784c786a06dc1c808cdb18a54136
Jeff Lund <jlund3@aml.cs.byu.edu>
2012-05-21 13:30:35 -0600

    Fleshed out naive bayes a bit

--- pytopic/classify/naive.py
+++ pytopic/classify/naive.py
@@ -1,24 +1,43 @@
 from __future__ import division
 
+import math
+
 class NaiveBayes(object):
 
-    def __init__(self, labels, data):
+    def __init__(self, labels, data, smooth=1):
         label_set = set(labels)
-        word_set = set()
-        for doc in data:
-            word_set.update(doc)
+        word_set = reduce(set.union, data, set())
 
         self.label_counts = {l: 0 for l in label_set}
-        self.word_counts = {l: {w: 0 for w in word_set} for l in label_set}
-
-        for label, doc in data:
+        for label in labels:
             self.label_counts[label] += 1
+        norm = sum(self.label_counts.values())
+        for label in label_set:
+            self.label_counts[label] /= norm
+            self.label_counts[label] = math.log(self.label_counts[label])
+
+        self.word_counts = {l: {w: 0 for w in word_set} for l in label_set}
+        for label, doc in zip(labels, data):
             for word in doc:
                 self.word_counts[label][word] += 1
-
-        label_norm = len(data)
-        for label in label_set:
-            self.label_counts[label] /= label_norm
-            word_norm = len(self.word_counts[label])
+        for label in labels:
+            counter = self.word_counts[label]
+            norm = sum(counter.values())
             for word in word_set:
-                self.word_counts[label][word] /= word_norm
+                counter[word] /= norm
+                if counter[word] == 0:
+                    counter[word] = float('-inf')
+                else:
+                    counter[word] = math.log(counter[word])
+
+    def _log_prior(self, label):
+        return self.label_counts[label]
+
+    def _log_likelihood(self, label, data):
+        likelihood = 0
+        for word in data:
+            likelihood += self.word_counts[label][word]
+        return likelihood
+
+    def _log_posterior(self, label, data):
+        return self._log_likelihood(label, data) + self._log_prior(label)
