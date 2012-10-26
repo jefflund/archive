744938100a4518e48ce8c7ad85859b3e83f1db1f
Jeff Lund <jefflund@gmail.com>
2012-10-26 16:36:32 -0600

    Restarting 20 newsgroups lda script

---
+++ scripts/newsgroups.py
@@ -0,0 +1,19 @@
+from pytopic.pipeline.corpus import CorpusReader
+from pytopic.pipeline.tokenizer import NewsTokenizer
+from pytopic.pipeline.preprocess import load_stopwords, filter_stopwords
+from pytopic.topic.vanilla import VanillaLDA
+
+def get_corpus():
+    reader = CorpusReader(NewsTokenizer())
+    reader.add_dir('../data/newsgroups/groups')
+    corpus = reader.read()
+
+    stopwords = load_stopwords('../data/stopwords/english.txt')
+    stopwords.add('writes')
+    stopwords.add('article')
+    corpus = filter_stopwords(corpus, stopwords)
+
+    return corpus
+
+if __name__ == '__main__':
+    corpus = get_corpus()
