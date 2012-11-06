9e01c6bda6a72bac9b7a0482f30be2d963a8a829
Jeff Lund <jefflund@gmail.com>
2012-11-06 12:04:09 -0700

    Added script for loading the bible

---
+++ scripts/bible.py
@@ -0,0 +1,16 @@
+from pytopic.pipeline.corpus import CorpusReader
+from pytopic.pipeline.tokenizer import BibleTokenizer
+from pytopic.pipeline.preprocess import load_stopwords, filter_stopwords
+
+def get_corpus():
+    reader = CorpusReader(BibleTokenizer())
+    reader.add_file('../data/bible/bible.txt')
+    corpus = reader.read()
+
+    stopwords = load_stopwords('../data/stopwords/english.txt')
+    corpus = filter_stopwords(corpus, stopwords)
+
+    return corpus
+
+if __name__ == '__main__':
+    corpus = get_corpus()
