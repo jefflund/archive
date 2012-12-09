f7500203d76277b1b6418c3ee219a0ade387e0a4
Jeff Lund <jefflund@gmail.com>
2012-12-08 22:36:26 -0700

    Minro bug fix in newsgroups script

--- scripts/newsgroups.py
+++ scripts/newsgroups.py
@@ -1,13 +1,13 @@
-from pytopic.pipeline.corpus import Corpus
-from pytopic.pipeline.tokenizer import NewsReader
+from pytopic.pipeline.corpus import CorpusReader
+from pytopic.pipeline.tokenizer import NewsTokenizer
 from pytopic.pipeline.preprocess import load_stopwords, filter_stopwords
 from pytopic.util.data import pickle_cache
-from pytopic.model.vanilla import VanillaLDA
-from pytopic.util.data import Printer, Timer()
+from pytopic.topic.vanilla import VanillaLDA
+from pytopic.util.handler import Printer, Timer
 
 @pickle_cache('../pickle/newsgroups-corpus.pickle')
 def get_corpus():
-    reader = CorpusReader(NewsReader())
+    reader = CorpusReader(NewsTokenizer())
     reader.add_dir('../data/newsgroups/groups')
     corpus = reader.read()
 
@@ -19,12 +19,12 @@ def get_corpus():
     return corpus
 
 def get_model(corpus):
-    lda = VanillaLDA(20, .4, .01)
+    lda = VanillaLDA(corpus, 20, .4, .01)
     lda.register_handler(Timer())
     lda.register_handler(Printer(10))
     return lda
 
 if __name__ == '__main__':
     corpus = get_corpus()
-    lda = get_model()
+    lda = get_model(corpus)
     lda.inference(100)
