45af0132e29dbcbbbde83aa565ce3bc4b93a22d5
Jeff Lund <jefflund@gmail.com>
2013-02-09 11:47:13 -0700

    Added argparse to newsgroups

--- scripts/kdd13/newsgroups-mm.py
+++ scripts/kdd13/newsgroups-mm.py
@@ -1,22 +1,38 @@
+import argparse
 from scripts.kdd13.corpora import get_newsgroups
 from pytopic.model.mixmulti import MixtureMultinomial
 from pytopic.util.handler import Printer, Timer, ClusterMetrics, Perplexity
 from pytopic.analysis.cluster import Clustering
 from pytopic.pipeline.preprocess import split_corpus
 
-def get_model(corpus, iter_interval):
-    training, test = split_corpus(corpus, .5)
+def get_model(corpus, opts):
+    training, test = split_corpus(corpus, opts.train_percent)
     clustering = Clustering.from_corpus(training)
+
     mm = MixtureMultinomial(training, 20, 2, .001)
-    mm.set_inference('gibbs')
+    mm.set_inference(*opts.inference)
+
     mm.register_handler(Timer())
-    mm.register_handler(Printer(iter_interval))
-    mm.register_handler(ClusterMetrics(clustering, iter_interval))
-    mm.register_handler(Perplexity(test, iter_interval))
+    mm.register_handler(Printer(opts.print_interval))
+    mm.register_handler(ClusterMetrics(clustering, opts.print_interval))
+    mm.register_handler(Perplexity(training, opts.print_interval))
+    mm.register_handler(Perplexity(test, opts.print_interval))
+
     return mm
 
+def get_opts():
+    parser = argparse.ArgumentParser(description='Runs MM on 20NG')
+    parser.add_argument('--inference', nargs='+', default=['gibbs'])
+    parser.add_argument('--num-iters', type=int, default=100)
+    parser.add_argument('--print-interval', type=int, default=10)
+    parser.add_argument('--train-percent', type=float, default=.5)
+    return parser.parse_args()
+
+def main():
+    opts = get_opts()
+    corpus = get_newsgroups()
+    model = get_model(corpus, opts)
+    model.inference(opts.num_iters)
 
 if __name__ == '__main__':
-    corpus = get_newsgroups()
-    mm = get_model(corpus, 10)
-    mm.inference(100)
+    main()
