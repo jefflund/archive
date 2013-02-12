d7118285abd8c78680d002133c33502ddb38af46
Jeff Lund <jefflund@gmail.com>
2013-02-11 21:14:28 -0700

    Used index clustering for enron

--- scripts/kdd13/enron.py
+++ scripts/kdd13/enron.py
@@ -4,4 +4,9 @@ from scripts.kdd13.runner import main
 from scripts.kdd13.corpora import get_enron
 from pytopic.analysis.cluster import Clustering
 
-main('Enron', get_enron, Clustering.from_corpus)
+def enron_clustering(corpus):
+    return Clustering.from_indices(corpus, 
+                                   '../data/enron/indices/ldc_split/all',
+                                   '../data/enron')
+
+main('Enron', get_enron, enron_clustering)
