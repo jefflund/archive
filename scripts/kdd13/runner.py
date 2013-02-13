84c2522c8ebe3a7e06bc82fd14696c27e00ad29e
Jeff Lund <jefflund@gmail.com>
2013-02-13 14:03:25 -0700

    New pssh run which does all experiments on the same seed

--- scripts/kdd13/runner.py
+++ scripts/kdd13/runner.py
@@ -1,10 +1,12 @@
 import argparse
+import gc
 import os
+import random
 from pytopic.model.mixmulti import MixtureMultinomial
 from pytopic.util.handler import Printer, Timer, ClusterMetrics, Perplexity
 from pytopic.pipeline.preprocess import split_corpus
 
-def get_cli_opts(dataset_name, args=None):
+def get_opts(dataset_name, args=None):
     desc = 'Run mixture of multinomials on the {} dataset'.format(dataset_name)
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('--inference', action='store', default='gibbs')
@@ -12,39 +14,9 @@ def get_cli_opts(dataset_name, args=None):
     parser.add_argument('--print-interval', type=int, default=1)
     parser.add_argument('--train-percent', type=float, default=.8)
     parser.add_argument('--anneal', action='store_true', default=False)
+    parser.add_argument('--seed', type=int, default=None)
     opts = parser.parse_args(args)
     return opts
-
-
-def get_pssh_opts(dataset_name):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--no-anneal', action='store_true', default=False)
-    parser.add_argument('--no-unanneal', action='store_true', default=False)
-    opts = parser.parse_args()
-
-    assert not opts.no_anneal or not opts.no_unanneal
-
-    pssh_opts = []
-    if not opts.no_unanneal:
-        pssh_opts.extend([['--inference', 'gibbs'],
-                          ['--inference', 'map'],
-                          ['--inference', 'em'],
-                          ['--inference', 'vem']])
-    if not opts.no_anneal:
-        pssh_opts.extend([['--inference', 'gibbs', '--anneal'],
-                          ['--inference', 'map', '--anneal'],
-                          ['--inference', 'em', '--anneal'],
-                          ['--inference', 'vem', '--anneal']])
-
-    args = pssh_opts[int(os.environ['PSSH_NODENUM']) % len(pssh_opts)]
-    return get_cli_opts(dataset_name, args)
-
-
-def get_opts(dataset_name):
-    if 'PSSH_NODENUM' in os.environ:
-        return get_pssh_opts(dataset_name)
-    else:
-        return get_cli_opts(dataset_name)
 
 
 def get_model(training, test, clustering, opts):
@@ -64,11 +36,42 @@ def print_opts(opts):
         header += 'annealed '
     header += opts.inference
     print header
+    print 'seed:', opts.seed
 
 
 def main(dataset_name, corpus_func, clustering_func):
-    opts = get_opts(dataset_name)
+    if 'PSSH_NODENUM' in os.environ:
+        pssh_main(dataset_name, corpus_func, clustering_func)
+    else:
+        opts = get_opts(dataset_name)
+        run(opts, corpus_func, clustering_func)
+
+
+def pssh_main(dataset_name, corpus_func, clustering_func):
+    seed = str(int(random.getrandbits(32)))
+    pssh_opts = [['--inference', 'gibbs', '--seed', seed],
+                 ['--inference', 'map', '--seed', seed],
+                 ['--inference', 'em', '--seed', seed],
+                 ['--inference', 'vem', '--seed', seed],
+                 ['--anneal', '--inference', 'gibbs', '--seed', seed],
+                 ['--anneal', '--inference', 'map', '--seed', seed],
+                 ['--anneal', '--inference', 'em', '--seed', seed],
+                 ['--anneal', '--inference', 'vem', '--seed', seed]]
+
+    # so I can graph as data comes in from pssh
+    node_num = int(os.environ['PSSH_NODENUM'])
+    run_num = node_num % len(pssh_opts)
+    pssh_opts = pssh_opts[run_num:] + pssh_opts[:run_num]
+
+    for opts in pssh_opts:
+        opts = get_opts(dataset_name, opts)
+        run(opts, corpus_func, clustering_func)
+        gc.collect()
+
+
+def run(opts, corpus_func, clustering_func):
     print_opts(opts)
+    random.seed(opts.seed)
 
     training, test = split_corpus(corpus_func(), opts.train_percent)
     clustering = clustering_func(training)
@@ -76,15 +79,15 @@ def main(dataset_name, corpus_func, clustering_func):
 
     if opts.anneal:
         opts.inference = 'annealed {}'.format(opts.inference)
-        opts.num_iters //= 4
+        #opts.num_iters //= 4
         model.set_inference(opts.inference, 25)
         model.inference(opts.num_iters)
-        model.set_inference(opts.inference, 10)
-        model.inference(opts.num_iters)
-        model.set_inference(opts.inference, 5)
-        model.inference(opts.num_iters)
-        model.set_inference(opts.inference, 1)
-        model.inference(opts.num_iters)
+        #model.set_inference(opts.inference, 10)
+        #model.inference(opts.num_iters)
+        #model.set_inference(opts.inference, 5)
+        #model.inference(opts.num_iters)
+        #model.set_inference(opts.inference, 1)
+        #model.inference(opts.num_iters)
     else:
         model.set_inference(opts.inference)
         model.inference(opts.num_iters)
