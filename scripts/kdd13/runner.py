7a23d7c47022dc98b16f99bd10c2063eab575224
Jeff Lund <jefflund@gmail.com>
2013-02-12 21:18:17 -0700

    Hopefully this will be the final runner tweak

--- scripts/kdd13/runner.py
+++ scripts/kdd13/runner.py
@@ -4,32 +4,27 @@ from pytopic.model.mixmulti import MixtureMultinomial
 from pytopic.util.handler import Printer, Timer, ClusterMetrics, Perplexity
 from pytopic.pipeline.preprocess import split_corpus
 
-def convert_param(param):
-    if param.isdigit():
-        return int(param)
-    elif param.isalpha():
-        return param.lower() in ['true', 'yes']
-    else:
-        return float(param)
-
-
 def get_cli_opts(dataset_name, args=None):
     desc = 'Run mixture of multinomials on the {} dataset'.format(dataset_name)
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('--inference', nargs='+', default=['gibbs'])
-    parser.add_argument('--num-iters', type=int, default=1000)
-    parser.add_argument('--print-interval', type=int, default=10)
+    parser.add_argument('--inference', action='store', default='gibbs')
+    parser.add_argument('--num-iters', type=int, default=100)
+    parser.add_argument('--print-interval', type=int, default=1)
     parser.add_argument('--train-percent', type=float, default=.8)
+    parser.add_argument('--anneal', action='store_true', default=False)
     opts = parser.parse_args(args)
-    opts.inference[1:] = [convert_param(param) for param in opts.inference[1:]]
     return opts
 
 
 def get_pssh_opts(dataset_name):
-    pssh_opts = [['--inference', 'annealed gibbs', '2'],
-                 ['--inference', 'annealed map', '2'],
-                 ['--inference', 'annealed em', '2'],
-                 ['--inference', 'annealed vem', '2']]
+    pssh_opts = [['--inference', 'gibbs'],
+                 ['--inference', 'gibbs', '--anneal'],
+                 ['--inference', 'map'],
+                 ['--inference', 'map', '--anneal'],
+                 ['--inference', 'em'],
+                 ['--inference', 'em', '--anneal'],
+                 ['--inference', 'vem'],
+                 ['--inference', 'vem', '--anneal']]
     args = pssh_opts[int(os.environ['PSSH_NODENUM']) % len(pssh_opts)]
     return get_cli_opts(dataset_name, args)
 
@@ -43,7 +38,6 @@ def get_opts(dataset_name):
 def get_model(training, test, clustering, opts):
     K = len(clustering.labels)
     model = MixtureMultinomial(training, K, 2, .001)
-    model.set_inference(*opts.inference)
 
     model.register_handler(Timer())
     model.register_handler(ClusterMetrics(clustering, opts.print_interval))
@@ -52,8 +46,11 @@ def get_model(training, test, clustering, opts):
     return model
 
 def print_opts(opts):
-    for key, value in vars(opts).iteritems():
-        print '{}: {}'.format(key, value)
+    header = 'inference: '
+    if opts.anneal:
+        header += 'annealed '
+    header += opts.inference
+    print header
 
 def main(dataset_name, corpus_func, clustering_func):
     opts = get_opts(dataset_name)
@@ -63,4 +60,16 @@ def main(dataset_name, corpus_func, clustering_func):
     clustering = clustering_func(training)
     model = get_model(training, test, clustering, opts)
 
-    model.inference(opts.num_iters)
+    if opts.anneal:
+        opts.inference = 'annealed {}'.format(opts.inference)
+        model.set_inference(opts.inference, 25)
+        model.inference(opts.num_iters)
+        model.set_inference(opts.inference, 10)
+        model.inference(opts.num_iters)
+        model.set_inference(opts.inference, 5)
+        model.inference(opts.num_iters)
+        model.set_inference(opts.inference, 1)
+        model.inference(opts.num_iters)
+    else:
+        model.set_inference(opts.inference)
+        model.inference(opts.num_iters)
