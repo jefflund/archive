db54a42647bffdd0c6d9e3b62baaa06213f6257a
Jeff Lund <jefflund@gmail.com>
2013-02-17 00:13:59 -0700

    Modified webkb runner script

--- scripts/kdd13/webkb.py
+++ scripts/kdd13/webkb.py
@@ -1,7 +1,26 @@
 #!/usr/bin/pypy
 
+import os
 from scripts.kdd13.runner import main
 from scripts.kdd13.corpora import get_webkb
 from pytopic.analysis.cluster import Clustering
 
-main('WebKB', get_webkb, Clustering.from_corpus)
+WEBKB_CLUSTERS = ['course',
+                  'department',
+                  'faculty',
+                  'other',
+                  'project',
+                  'staff',
+                  'student']
+
+def find_cluster(title):
+    for cluster in WEBKB_CLUSTERS:
+        if 'webkb' + os.sep + cluster in title:
+        if cluster in title:
+            return cluster
+
+def webkb_clustering(corpus):
+    data =  [find_cluster(title) for title in corpus.titles]
+    return Clustering(WEBKB_CLUSTERS, data)
+
+main('WebKB', get_webkb, webkb_clustering)
