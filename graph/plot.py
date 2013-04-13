d78c561342a84ab7e96ee4ac3f018f88b621cd8e
Jeff Lund <jefflund@gmail.com>
2013-04-13 10:26:50 -0600

    Added aggregation functions in plot

--- graph/plot.py
+++ graph/plot.py
@@ -1,6 +1,14 @@
 import pylab
 
-class RunData(object):
+def mean(values):
+    return sum(values) / len(values)
+
+def median(values):
+    values = sorted(values)
+    half_n = len(values) // 2
+    return (values[half_n] + values[-half_n]) / 2
+
+class Plotter(object):
 
     def __init__(self):
         self.data = {}
@@ -25,3 +33,12 @@ class RunData(object):
             pylab.plot(xdata, ydata, linewidth=1, label=ykey)
 
         pylab.show()
+
+    @classmethod
+    def aggregate(cls, plotters, aggregate_func=median, **overrides):
+        data = {}
+        for plotter in plotters:
+            for key, values in plotter.data.iteritems():
+                if key not in data:
+                    data[key] = []
+                data[key].append(values)
