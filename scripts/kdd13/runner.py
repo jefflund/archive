722015d050dd11f2b87ba9a3cd468b2716005229
Jeff Lund <jefflund@gmail.com>
2013-05-03 16:46:01 -0600

    Runner now does annealed versions

--- scripts/kdd13/runner.py
+++ scripts/kdd13/runner.py
@@ -1,9 +1,18 @@
+#!/usr/bin/pypy
+
 from pytopic.model import mixmulti
 from pytopic.util import handler
 from scripts.corpora import newsgroups
 
+K = 20
+GAMMA = 2
+BETA = .001
+
+RUN_TIME = 1200
+ANNEAL_SCHEDULE = [25, 10, 5, 1]
+
 def get_model(corpus, clustering):
-    model = mixmulti.MixtureMultinomial(corpus, 20, 2, .001)
+    model = mixmulti.MixtureMultinomial(corpus, K, GAMMA, BETA)
     model.register_handler(handler.ClusterConvergeceCheck(model.k))
     model.register_handler(handler.StateTimePrinter(model.k))
     return model
@@ -12,8 +21,15 @@ def run(corpus, clustering, inference):
     model = get_model(corpus, clustering)
     print 'params', inference
     model.set_inference(inference)
-    model.timed_inference(1200)
+    model.timed_inference(RUN_TIME)
 
+def annealed_run(corpus, clustering, inference):
+    model = get_model(corpus, clustering)
+    print 'params', 'annealed', inference
+
+    for temp in ANNEAL_SCHEDULE:
+        model.set_inference(inference, 25)
+        model.timed_inference(RUN_TIME / len(ANNEAL_SCHEDULE))
 
 if __name__ == '__main__':
     corpus = newsgroups.get_corpus()
@@ -22,3 +38,9 @@ if __name__ == '__main__':
 
     run(corpus, clustering, 'ccm')
     run(corpus, clustering, 'em')
+    run(corpus, clustering, 'vem')
+    run(corpus, clustering, 'gibbs')
+    
+    annealed_run(corpus, clustering, 'annealed gibbs')
+    annealed_run(corpus, clustering, 'annealed em')
+    annealed_run(corpus, clustering, 'annealed vem')
