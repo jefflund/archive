224bec8b2f05b71626a59688e4f43a763ba23e75
Jeff Lund <jefflund@gmail.com>
2013-06-25 13:53:18 -0700

    Started move towards thesis cluster run on multiple datasets

--- scripts/thesis/run/cluster.py
+++ scripts/thesis/run/cluster.py
@@ -1,24 +1,31 @@
 #!/usr/bin/pypy
 
+import argparse
 from pytopic.model import mixmulti
 from pytopic.util import handler
 from scripts.corpora import newsgroups
 
-K = 20
-GAMMA = 2
-BETA = .001
-
 RUN_TIME = 1200
 ANNEAL_SCHEDULE = [25, 10, 5, 1]
+INFERENCE_ALGORITHMS = ['ccm', 'gibbs', 'vem', 'em',
+                        'annealed gibbs', 'annealed vem', 'annealed em']
 
-def get_model(corpus, clustering):
-    model = mixmulti.MixtureMultinomial(corpus, K, GAMMA, BETA)
+OPTS = {'20ng': tuple()}
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--corpus', default='20ng',
+                        help='Corpus to use ({})'.format(', '.join(OPTS)))
+    return parser.parse_args()
+
+def get_model(corpus, clustering, K, gamma, beta):
+    model = mixmulti.MixtureMultinomial(corpus, K, gamma, beta)
     model.register_handler(handler.ClusterConvergeceCheck(model.k))
     model.register_handler(handler.StateTimePrinter(model.k))
     return model
 
 def run(corpus, clustering, inference):
-    model = get_model(corpus, clustering)
+    model = get_model(corpus, clustering, 20, 2, .001)
     print 'params', inference
 
     if inference.startswith('annealed'):
@@ -30,15 +37,12 @@ def run(corpus, clustering, inference):
         model.timed_inference(RUN_TIME)
 
 if __name__ == '__main__':
+    args = get_args()
+    opts = OPTS[args.corpus]
+
     corpus = newsgroups.get_corpus()
     clustering = newsgroups.get_clustering(corpus)
     print 'gold', repr(clustering.data)
 
-    run(corpus, clustering, 'ccm')
-    run(corpus, clustering, 'em')
-    run(corpus, clustering, 'vem')
-    run(corpus, clustering, 'gibbs')
-
-    run(corpus, clustering, 'annealed gibbs')
-    run(corpus, clustering, 'annealed em')
-    run(corpus, clustering, 'annealed vem')
+    for algorithm in INFERENCE_ALGORITHMS:
+        run(corpus, clustering, algorithm)
