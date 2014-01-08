f3f61c87b142521797cc4c5494be358fc20cb556
Jeffrey Lund <jefflund@gmail.com>
2014-01-08 15:07:28 -0700

    Added fmeasure plot shortcut

--- thesis.py
+++ thesis.py
@@ -2,7 +2,7 @@ import os
 
 import evilplot
 
-def time_metric(results_dir, time_col, metric_col, **kwargs):
+def time_metric_plot(results_dir, time_col, metric_col, **kwargs):
     plot = evilplot.Plot(**kwargs)
     for root, _, filenames in os.walk(results_dir):
         linename = os.path.basename(root)
@@ -10,3 +10,11 @@ def time_metric(results_dir, time_col, metric_col, **kwargs):
             with open(os.path.join(root, filename)) as columns:
                 plot.add_from_column(linename, columns, time_col, metric_col)
     return plot
+
+
+def show_fmeasure_plot(results_dir, dataset_name):
+    title = 'Mixture of Multinomials on {}'.format(dataset_name)
+    plot = time_metric_plot(results_dir, 0, 1, title=title,
+                                               xlabel='Time (seconds)',
+                                               ylabel='F-Measure')
+    plot.show(end_error=True)
