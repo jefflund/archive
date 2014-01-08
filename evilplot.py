e3a4fb7c4f124cef8b702ebf4c107a971b98d79e
Jeffrey Lund <jefflund@gmail.com>
2014-01-08 14:44:11 -0700

    Plots slightly prettier

--- evilplot.py
+++ evilplot.py
@@ -1,18 +1,13 @@
 from __future__ import division
 
 import pylab
-
-
-def mean(xs):
-    return sum(xs) / len(xs)
+import itertools
 
 
 class AggLine(object):
 
-    def __init__(self, agg_x=mean, agg_y=mean):
+    def __init__(self):
         self.lines = []
-        self.agg_x = agg_x
-        self.agg_y = agg_y
 
     def add_xys(self, xys):
         self.lines.append(xys)
@@ -26,8 +21,8 @@ class AggLine(object):
         self.add_xys(xys)
 
     def __getitem__(self, index):
-        x, y = zip(*(line[index] for line in self.lines if len(line) > index))
-        return self.agg_x(x), self.agg_y(y)
+        x, y = zip(*(l[index if len(l) > index else -1] for l in self.lines))
+        return pylab.mean(x), pylab.mean(y)
 
     def __len__(self):
         return max(len(line) for line in self.lines)
@@ -36,22 +31,27 @@ class AggLine(object):
         for i in range(len(self)):
             yield self[i]
 
+    def plot(self, style='-', **kwargs):
+        x, y = zip(*self)
+        pylab.plot(x, y, style, **kwargs)
+
 
 class Plot(object):
 
-    def __init__(self, agg_x=mean, agg_y=mean, **kwargs):
+    def __init__(self, **kwargs):
         self.lines = {}
-        self.agg_x = agg_x
-        self.agg_y = agg_y
 
         self.xlabel = kwargs.get('xlabel', 'X')
         self.ylabel = kwargs.get('ylabel', 'Y')
         self.legend = kwargs.get('legend', 'best')
         self.title = kwargs.get('title')
 
+        self.color_cycle = ['#00305E', '#007722', '#990011']
+        self.style_cycle = ['solid', 'dashed', 'dotted', 'dash-dot']
+
     def __getitem__(self, name):
         if name not in self.lines:
-            self.lines[name] = AggLine(self.agg_x, self.agg_y)
+            self.lines[name] = AggLine()
         return self.lines[name]
 
     def add_xys(self, name, xys):
@@ -61,9 +61,10 @@ class Plot(object):
         self[name].add_from_column(columns, col_x, col_y)
 
     def show(self):
+        get_color = itertools.cycle(self.color_cycle).next
+        get_style = itertools.cycle(self.style_cycle).next
         for name, line in self.lines.iteritems():
-            x, y = zip(*line)
-            pylab.plot(x, y, label=name)
+            line.plot(label=name, color=get_color(), linestyle=get_style())
 
         self._show_opt('xlabel')
         self._show_opt('ylabel')
