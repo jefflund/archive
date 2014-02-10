300fb5179e0a24ceeef110084226adad9d91fc3d
Jeffrey Lund <jefflund@gmail.com>
2014-02-10 11:58:20 -0700

    Added ImportIndex and Clustering

--- dataset/dataset.go
+++ dataset/dataset.go
@@ -6,7 +6,7 @@ import (
 )
 
 type ImportSpec struct {
-	Root      string
+	Path      string
 	Tokenizer *pipeline.Tokenizer
 	Stopwords []string
 }
@@ -14,7 +14,7 @@ type ImportSpec struct {
 func ImportDir(s ImportSpec) *pipeline.Corpus {
 	corpus := pipeline.NewCorpus()
 
-	err := pipeline.UpdateCorpusWalk(s.Root, s.Tokenizer, corpus)
+	err := pipeline.UpdateCorpusWalk(s.Path, s.Tokenizer, corpus)
 	if err != nil {
 		panic(err)
 	}
@@ -25,4 +25,28 @@ func ImportDir(s ImportSpec) *pipeline.Corpus {
 	}
 
 	return corpus
+}
+
+func ImportDirClustering(c *pipeline.Corpus, _ ImportSpec) *eval.Clustering {
+	return eval.NewClusteringTitle(c)
+}
+
+func ImportIndex(s ImportSpec) {
+	corpus := pipeline.NewCorpus()
+
+	err := pipeline.UpdateCorpusIndex(s.Path, s.Tokenizer, corpus)
+	if err != nil {
+		panic(err)
+	}
+
+	corpus, err = pipeline.FilterStopwords(corpus, s.Stopwords...)
+	if err != nil {
+		panic(err)
+	}
+
+	return corpus
+}
+
+func ImportIndexClustering(c *pipeline.Corpus, s ImportSpec) *eval.Clustering {
+	return eval.NewClusteringIndex(s.Path, c)
 }
