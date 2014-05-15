752cdc340825b05676315e86659527b6711a80ea
Jeffrey Lund <jefflund@gmail.com>
2014-05-15 16:17:20 -0600

    Renamed files for clarity

--- load/dataset.go
+++ load/dataset.go
@@ -1,77 +1,61 @@
 package load
 
 import (
-	"github.com/jlund3/modelt/eval"
+	"fmt"
+
 	"github.com/jlund3/modelt/pipeline"
 )
 
-type ImportSpec struct {
-	Path      string
-	Tokenizer pipeline.Tokenizer
-	Stopwords []string
-	Stem      bool
-	Common    float64
+var (
+	Newsgroups = Importer{
+		ImportSpec{
+			"data/newsgroups",
+			pipeline.NewsTokenizer,
+			[]string{
+				"data/stopwords/newsgroups.txt",
+				"data/stopwords/english.txt"},
+			false, 0},
+		ImportDir,
+		ImportDirClustering}
+
+	Enron = Importer{
+		ImportSpec{
+			"data/enron/annotation.tab",
+			pipeline.NewsTokenizer,
+			[]string{"data/stopwords/english.txt"},
+			false, 0},
+		ImportIndex,
+		ImportIndexClustering}
+
+	Ambiant  = createAmbiantImporters()
+	Moresque = createMoresqueImporters()
+)
+
+func createQueryImporter(filename string) Importer {
+	return Importer{
+		ImportSpec{
+			filename,
+			pipeline.BasicTokenizer,
+			[]string{"data/stopwords/english.txt"},
+			true, .8},
+		ImportDir,
+		ImportDirClustering}
 }
 
-type Importer struct {
-	spec     ImportSpec
-	importer func(ImportSpec) *pipeline.Corpus
-	labeler  func(*pipeline.Corpus, ImportSpec) *eval.Clustering
-}
-
-func (i Importer) Import() *pipeline.Corpus {
-	return i.importer(i.spec)
-}
-
-func (i Importer) Label(c *pipeline.Corpus) *eval.Clustering {
-	return i.labeler(c, i.spec)
-}
-
-func ImportDir(s ImportSpec) *pipeline.Corpus {
-	corpus := pipeline.NewCorpus()
-	err := pipeline.UpdateCorpusWalk(s.Path, s.Tokenizer, corpus)
-	if err != nil {
-		panic(err)
+func createAmbiantImporters() []Importer {
+	importers := make([]Importer, 44)
+	for i := 0; i < 44; i++ {
+		filename := fmt.Sprintf("data/ambiant/data/%d", i+1)
+		importers[i] = createQueryImporter(filename)
 	}
-	return runImportSpec(corpus, s)
+	return importers
 }
 
-func ImportIndex(s ImportSpec) *pipeline.Corpus {
-	corpus := pipeline.NewCorpus()
-	err := pipeline.UpdateCorpusIndex(s.Path, s.Tokenizer, corpus)
-	if err != nil {
-		panic(err)
+func createMoresqueImporters() []Importer {
+	importers := make([]Importer, 114)
+	for i := 0; i < 114; i++ {
+		filename := fmt.Sprintf("data/moresque/data/%d", i+45)
+		importers[i] = createQueryImporter(filename)
 	}
-	return runImportSpec(corpus, s)
-}
-
-func runImportSpec(corpus *pipeline.Corpus, s ImportSpec) *pipeline.Corpus {
-	corpus, err := pipeline.FilterStopwords(corpus, s.Stopwords...)
-	if err != nil {
-		panic(err)
-	}
-
-	if s.Stem {
-		corpus, err = pipeline.ApplyStemming(corpus)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	if s.Common > 0 {
-		corpus, err = pipeline.RemoveCommon(corpus, s.Common)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	return corpus
-}
-
-func ImportDirClustering(c *pipeline.Corpus, _ ImportSpec) *eval.Clustering {
-	return eval.NewClusteringTitle(c)
-}
-
-func ImportIndexClustering(c *pipeline.Corpus, s ImportSpec) *eval.Clustering {
-	return eval.NewClusteringIndexRef(s.Path, c)
+	return importers
 }
---
+++ load/spec.go
@@ -0,0 +1,77 @@
+package load
+
+import (
+	"github.com/jlund3/modelt/eval"
+	"github.com/jlund3/modelt/pipeline"
+)
+
+type ImportSpec struct {
+	Path      string
+	Tokenizer pipeline.Tokenizer
+	Stopwords []string
+	Stem      bool
+	Common    float64
+}
+
+type Importer struct {
+	spec     ImportSpec
+	importer func(ImportSpec) *pipeline.Corpus
+	labeler  func(*pipeline.Corpus, ImportSpec) *eval.Clustering
+}
+
+func (i Importer) Import() *pipeline.Corpus {
+	return i.importer(i.spec)
+}
+
+func (i Importer) Label(c *pipeline.Corpus) *eval.Clustering {
+	return i.labeler(c, i.spec)
+}
+
+func ImportDir(s ImportSpec) *pipeline.Corpus {
+	corpus := pipeline.NewCorpus()
+	err := pipeline.UpdateCorpusWalk(s.Path, s.Tokenizer, corpus)
+	if err != nil {
+		panic(err)
+	}
+	return runImportSpec(corpus, s)
+}
+
+func ImportIndex(s ImportSpec) *pipeline.Corpus {
+	corpus := pipeline.NewCorpus()
+	err := pipeline.UpdateCorpusIndex(s.Path, s.Tokenizer, corpus)
+	if err != nil {
+		panic(err)
+	}
+	return runImportSpec(corpus, s)
+}
+
+func runImportSpec(corpus *pipeline.Corpus, s ImportSpec) *pipeline.Corpus {
+	corpus, err := pipeline.FilterStopwords(corpus, s.Stopwords...)
+	if err != nil {
+		panic(err)
+	}
+
+	if s.Stem {
+		corpus, err = pipeline.ApplyStemming(corpus)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if s.Common > 0 {
+		corpus, err = pipeline.RemoveCommon(corpus, s.Common)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return corpus
+}
+
+func ImportDirClustering(c *pipeline.Corpus, _ ImportSpec) *eval.Clustering {
+	return eval.NewClusteringTitle(c)
+}
+
+func ImportIndexClustering(c *pipeline.Corpus, s ImportSpec) *eval.Clustering {
+	return eval.NewClusteringIndexRef(s.Path, c)
+}
