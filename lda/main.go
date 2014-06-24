eed944f7317ebf579b55d969de0092b3b8a40049
Jeffrey Lund <jefflund@gmail.com>
2014-06-24 17:39:18 -0600

    LDA-NB experiment

--- lda/main.go
+++ lda/main.go
@@ -6,8 +6,7 @@ import (
 	"time"
 
 	"github.com/jlund3/modelt/eval"
-	"github.com/jlund3/modelt/topic"
-	"github.com/jlund3/modelt/topic/interactive"
+	"github.com/jlund3/modelt/topic/vanilla"
 
 	"github.com/jlund3/ford/load"
 )
@@ -16,81 +15,29 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	corpus := load.Newsgroups.Import()
-	rounds, constraints := load.GetConstraints("data/constraints/newsgroups.txt")
+	model := vanilla.NewLDA(corpus, 4, .1, .01)
 
-	model := interactive.NewITM(corpus, 20, .1, .01, 100)
+	inference := vanilla.NBTrain(model, .975)
 
-	evaluate := func(name string, round int) {
+	for i := 0; i < 100; i++ {
+		start := time.Now()
+		inference()
+		end := time.Now()
+
+		fmt.Println("Iteration:", i, "Time:", end.Sub(start))
+
+		start = time.Now()
 		labeled := eval.NewLabeledCorpusModel(model)
 		train, test := labeled.SplitRand(.9)
 		naive := eval.NewNaiveBayes(train)
-		fmt.Println(name, round, ":", naive.Validate(test))
-	}
+		fmt.Println("Accuracy:", naive.Validate(test))
+		end = time.Now()
+		fmt.Println("NB Time:", end.Sub(start))
+		fmt.Println("Posterior:", model.Posterior())
 
-	inference := interactive.Gibbs(model)
-	for i := 0; i < 100; i++ {
-		inference()
-	}
-	base := model.CopyState()
-	evaluate("Base", 0)
-
-	newModel := func() *interactive.ITM {
-		i := interactive.NewITM(corpus, 20, .1, .01, 100)
-		i.SetState(base)
-		return i
-	}
-
-	model = newModel()
-	inference = interactive.Gibbs(model)
-	for round := 1; round < rounds; round++ {
-		for _, constraint := range constraints {
-			model.AddConstraintString(constraint[:round])
+		fmt.Println("Topics:")
+		for z := 0; z < model.T; z++ {
+			fmt.Println(z, model.Topics[z], model.TopicSummary(z, 10))
 		}
-		for i := 0; i < 10; i++ {
-			inference()
-		}
-		evaluate("Gibbs", round)
-	}
-
-	model = newModel()
-	inference = interactive.CMM(model)
-	for round := 1; round < rounds; round++ {
-		for _, constraint := range constraints {
-			model.AddConstraintString(constraint[:round])
-		}
-		converged := false
-		checker := topic.NewWordConvergenceChecker(model.Z)
-		for !converged {
-			inference()
-			converged = checker.Check() == 0
-		}
-		evaluate("CCM", round)
-	}
-
-	model = newModel()
-	inference = interactive.CMM(model)
-	for round := 1; round < rounds; round++ {
-		for _, constraint := range constraints {
-			model.AddConstraintString(constraint[:round])
-		}
-		interactive.AbolateAll(model, nil)
-		inference()
-		evaluate("SUGS", round)
-	}
-
-	model = newModel()
-	inference = interactive.CMM(model)
-	for round := 1; round < rounds; round++ {
-		for _, constraint := range constraints {
-			model.AddConstraintString(constraint[:round])
-		}
-		interactive.AbolateAll(model, nil)
-		converged := false
-		checker := topic.NewWordConvergenceChecker(model.Z)
-		for !converged {
-			inference()
-			converged = checker.Check() == 0
-		}
-		evaluate("SUGS-CCM", round)
 	}
 }
