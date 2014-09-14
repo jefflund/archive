e05f34ed76cf73ac738f2b93bcf183b9403c3942
Jeffrey Lund <jefflund@gmail.com>
2014-09-13 19:29:34 -0600

    CCM clean experiment

--- cmd/lda/main.go
+++ cmd/lda/main.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/jlund3/ford/load"
 	"github.com/jlund3/modelt/eval"
-	//"github.com/jlund3/modelt/topic"
+	"github.com/jlund3/modelt/topic"
 	"github.com/jlund3/modelt/topic/vanilla"
 )
 
@@ -35,14 +35,15 @@ func main() {
 		panic(err)
 	}
 	defer out.Close()
-	fmt.Fprintln(out, "#iter secs post accur")
+	fmt.Fprintln(out, "#iter secs post accur ccm-secs ccm-post ccm-accur")
 
 	// Create model
 	corpus := load.Newsgroups.Import()
 	lda := vanilla.NewLDA(corpus, 20, .1, .01)
 
-	//ccm := vanilla.CCM(lda)
-	//gibbs := vanilla.Gibbs(lda)
+	ccm := vanilla.CCM(lda)
+	gibbs := vanilla.Gibbs(lda)
+	checker := topic.NewWordConvergenceChecker(lda.Z)
 	//angibbs := []topic.Inferencer{
 	//vanilla.AnnealedGibbs(lda, 3),
 	//vanilla.AnnealedGibbs(lda, 2.5),
@@ -50,27 +51,49 @@ func main() {
 	//vanilla.AnnealedGibbs(lda, 1.5),
 	//vanilla.AnnealedGibbs(lda, 1)}
 	//sel := vanilla.GreedySelect(lda)
-	softccm := vanilla.SoftWordCCM(lda, 5)
+	//softccm := vanilla.SoftWordCCM(lda, 5)
 
 	// Run inference
 	iters := 1000
-	pinter := iters / 10
-	start := time.Now()
+	var dur time.Duration
 	for i := 1; i <= iters; i++ {
-		softccm()
+		start := time.Now()
+		gibbs()
+		end := time.Now()
+		dur += end.Sub(start)
 
-		if i%pinter == 0 {
-			labeled := eval.NewLabeledCorpusModel(lda)
-			train, test := labeled.SplitRand(.8)
-			naive := eval.NewNaiveBayes(train)
+		checker.Check()
 
-			stats := []string{
-				fmt.Sprintf("%d", i),
-				fmt.Sprintf("%f", time.Now().Sub(start).Seconds()),
-				fmt.Sprintf("%f", lda.Posterior()),
-				fmt.Sprintf("%f", naive.Validate(test))}
-			fmt.Fprintln(out, strings.Join(stats, " "))
+		labeled := eval.NewLabeledCorpusModel(lda)
+		train, test := labeled.SplitRand(.8)
+		naive := eval.NewNaiveBayes(train)
+
+		stats := []string{
+			fmt.Sprintf("%d", i),
+			fmt.Sprintf("%f", dur.Seconds()),
+			fmt.Sprintf("%f", lda.Posterior()),
+			fmt.Sprintf("%f", naive.Validate(test))}
+
+		checker.Check()
+		start = time.Now()
+		ccm()
+		end = time.Now()
+
+		labeled = eval.NewLabeledCorpusModel(lda)
+		train, test = labeled.SplitRand(.8)
+		naive = eval.NewNaiveBayes(train)
+
+		stats = append(stats,
+			fmt.Sprintf("%f", (dur+end.Sub(start)).Seconds()),
+			fmt.Sprintf("%f", lda.Posterior()),
+			fmt.Sprintf("%f", naive.Validate(test)))
+
+		fmt.Fprintln(out, strings.Join(stats, " "))
+
+		for d := 0; d < lda.M; d++ {
+			for n := 0; n < lda.N[d]; n++ {
+				lda.SetZ(d, n, checker.State[d][n])
+			}
 		}
 	}
-
 }
