2237b7cb4d586ae23f08d3bec084eb5c5ced2990
Jeffrey Lund <jefflund@gmail.com>
2015-01-28 13:36:22 -0700

    Better color/marker styles

--- evilplot/line.py
+++ evilplot/line.py
@@ -3,9 +3,9 @@
 from __future__ import division
 
 import pylab
+import itertools
 
 from evilplot.plot import Plot
-
 
 class LinePlot(Plot):
     """A Plot with runs aggregated into lines"""
@@ -14,9 +14,13 @@ class LinePlot(Plot):
         Plot.__init__(self, **kwargs)
 
     def pylab_plot(self):
+        color = itertools.cycle(['b', 'g', 'r', 'm', 'y', 'k']).next
+        marker = itertools.cycle(['-', '--', '-.', ':']).next
+
         for name, line in self.iter_data():
             x, y = zip(*line)
-            pylab.plot(x, y, label=name)
+            style = color() + marker()
+            pylab.plot(x, y, style, label=name)
 
     def pgf_plot(self, prefix):
         for name, line in self.iter_data():
