4b6bb8d45a0123633deea3fc9b5191e143828d88
Jeffrey Lund <jefflund@gmail.com>
2015-06-24 08:45:00 -0600

    Fixed the face

--- escape/escape.cc
+++ escape/escape.cc
@@ -6,6 +6,6 @@ int main() {
   cout << "\tI can indent lines with escape sequences.\n";
   cout << "I can also print \"quoted\" things with escape sequences\n";
   cout << "I can even print silly stuff like\n\t\t";
-  cout << "(\u3065\uFF61\u25D5\u203F\u203F\u25D5\uFF61)\u3065";
+  cout << "(\u3065\uFF61\u25D5\u203F\u25D5\uFF61)\u3065";
   return 0;
 }
