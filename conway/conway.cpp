dc1429d6ea352cbb4c1490f3d78777d0079e8451
Jeffrey Lund <jefflund@gmail.com>
2015-07-16 08:05:49 -0600

    Slight tweaks to conway

--- conway/conway.cpp
+++ conway/conway.cpp
@@ -14,20 +14,10 @@ const int NUM_GENS = 100;
 // the given probability, and the boarder of the board set to dead.
 void init_board(bool board[BOARD_COLS][BOARD_ROWS], double alive_prob) {
   // fill the middle of the board, with each cell alive with alive_prob
-  for (int x = 1; x < BOARD_COLS - 1; x++) {
-    for (int y = 1; y < BOARD_ROWS - 1; y++) {
+  for (int x = 0; x < BOARD_COLS; x++) {
+    for (int y = 0; y < BOARD_ROWS; y++) {
       board[x][y] = (double)rand() / INT_MAX < alive_prob;
     }
-  }
-
-  // fill the board with dead cells
-  for (int x = 0; x < BOARD_COLS; x++) {
-    board[x][0] = false;
-    board[x][BOARD_ROWS - 1] = false;
-  }
-  for (int y = 0; y < BOARD_ROWS; y++) {
-    board[0][y] = false;
-    board[BOARD_COLS - 1][y] = false;
   }
 }
 
@@ -37,24 +27,50 @@ void delay(double seconds) {
   usleep(seconds * MICROS_PER_SEC);
 }
 
-// alive returns true if the cell at x, y will be alive in the next generation.
-bool alive(bool board[BOARD_COLS][BOARD_ROWS], int x, int y) {
-  // count the number of alive neighbors the cell has
+// computes x modulo y (not the same as remainder)
+int mod(int x, int y) {
+  int z = x % y;
+  if (z < 0) {
+    z += y;
+  }
+  return z;
+}
+
+// computes the number of living neighbors a cell has
+int alive_neighbors(bool board[BOARD_COLS][BOARD_ROWS], int x, int y) {
   int neighbors = 0;
+  // loop over every cell with in manhattan distance of 1.
   for (int dx = -1; dx <= 1; dx++) {
     for (int dy = -1; dy <= 1; dy++) {
-      // if the cell is alive, and its not ourself
-      if (board[x+dx][y+dy] && (dx != 0 || dy != 0)) {
+      // we aren't our own neighbor
+      if (dx == 0 && dy == 0) {
+        continue;
+      }
+
+      // compute the offsets, with wrapping around the edges
+      int adjx = mod(x + dx, BOARD_COLS);
+      int adjy = mod(y + dy, BOARD_ROWS);
+
+      // check if the adjacent cell is alive
+      if (board[adjx][adjy]) {
         neighbors++;
       }
     }
   }
 
+  return neighbors;
+}
+
+// alive returns true if the cell at x, y will be alive in the next generation.
+bool alive(bool board[BOARD_COLS][BOARD_ROWS], int x, int y) {
   // See https://en.wikipedia.org/wiki/Conway's_Game_of_Life for information
   // on these rules.
+  int neighbors = alive_neighbors(board, x, y);
   if (board[x][y]) {
+    // if already alive, need 2 or 3 neighbors to avoid under/over population
     return neighbors == 2 || neighbors == 3;
   } else {
+    // if dead, spawn if exactly 3 neighbors
     return neighbors == 3;
   }
 }
@@ -65,15 +81,15 @@ void update_board(bool board[BOARD_COLS][BOARD_ROWS]) {
   // if we update board in place, then we mix the current and next generation
   // of states in this computation.
   bool scratch[BOARD_COLS][BOARD_ROWS];
-  for (int x = 1; x < BOARD_COLS - 1; x++) {
-    for (int y = 1; y < BOARD_ROWS - 1; y++) {
+  for (int x = 0; x < BOARD_COLS; x++) {
+    for (int y = 0; y < BOARD_ROWS; y++) {
       scratch[x][y] = alive(board, x, y);
     }
   }
 
   // copy scratch into board so board contains the new generation
-  for (int x = 1; x < BOARD_COLS - 1; x++) {
-    for (int y = 1; y < BOARD_ROWS - 1; y++) {
+  for (int x = 0; x < BOARD_COLS; x++) {
+    for (int y = 0; y < BOARD_ROWS; y++) {
       board[x][y] = scratch[x][y];
     }
   }
@@ -83,16 +99,14 @@ void update_board(bool board[BOARD_COLS][BOARD_ROWS]) {
 void print_board(bool board[BOARD_COLS][BOARD_ROWS]) {
   cout << "\033[H"; // move the cursor to the top left
 
-  // y on the outer loop, so we print row by row
+  // y on the outer loop, so we print row by row, not by column
   for (int y = 0; y < BOARD_ROWS; y++) {
     // print each value of the row
     for (int x = 0; x < BOARD_COLS; x++) {
-      cout << (board[x][y] ? "O" : "\u00B7");
+      cout << (board[x][y] ? "\u25CF" : "\u00B7");
     }
     cout << endl; // advance to the next row
   }
-
-  cout << endl;
 }
 
 int main() {
@@ -103,7 +117,7 @@ int main() {
   init_board(board, .2);
   print_board(board);
 
-  for (int i = 0; i < NUM_GENS; i++) {
+  while (true) {
     update_board(board);
     print_board(board);
     delay(.1);
