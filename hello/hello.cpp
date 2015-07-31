552b283c6c75a1b29b25e91ee10296194b6c24e2
Jeffrey Lund <jefflund@gmail.com>
2015-07-31 08:42:07 -0600

    Revert hello (oops)

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Foo {
- public:
-  Foo();
-  virtual ~Foo();
-};
-
-Foo::Foo() {
-  cout << "constructed" << endl;
-}
-
-Foo::~Foo() {
-  cout << "delorted" << endl;
-}
-
-void foobius() {
-  Foo foo;
-}
-
 int main() {
-  foobius();
-  Foo* foo = new Foo();
-  delete foo;
-
+  cout << "Hello World" << endl;
   return 0;
 }
