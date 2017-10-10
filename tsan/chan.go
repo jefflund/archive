2fbc9f21a8e456090095fa97569e0d38b898ae83
Jeffrey Lund <jefflund@gmail.com>
2017-10-10 10:54:32 -0600

    More prep

---
+++ tsan/chan.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var names = []string{
+	"Alice",
+	"Bob",
+	"Carol",
+	"Dennis",
+	"Egbert",
+	"Farquad",
+	"George",
+	"Heather",
+	"Igrid",
+	"Jessica",
+}
+
+func main() {
+	wg := new(sync.WaitGroup)
+	people := make(chan string)
+	go func() {
+		for _, name := range names { // Pretend this came from file or network
+			wg.Add(1)
+			people <- name
+		}
+		close(people)
+	}()
+
+	match := make(chan string)
+
+	for name := range people {
+		name := name // Shadows the variable to avoid race condition
+		go func() {
+			select {
+			case peer := <-match:
+				fmt.Printf("%s matched with %s\n", name, peer)
+			case match <- name:
+				// wait for someone to get message
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
